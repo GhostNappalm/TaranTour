49f7a1f414913b6999e5f623269ec894888f856a
GhostNappalm <m.baso@studenti.uniba.it>
2023-03-17 16:08:16 +0100

    dwd

--- js/generalJS.js
+++ js/generalJS.js
@@ -29,7 +29,7 @@ $(".exit").click(function(){
 
 
     //Js per Help
-  /*  $('#help').click(function() {
+      $('#help').click(function() {
       $('#pu_hlp').fadeIn(200);
       $('#pop-up-help').show();
     });   
@@ -38,7 +38,7 @@ $(".exit").click(function(){
       $('#pu_hlp').hide();
       $('#pop-up-help').hide();
     });
-    */
+    
     var popup = document.getElementById("pu_ext");
 
     document.addEventListener("keydown", function(event) {

4239160cfbc71910677e5f898fe7ccedd60faa6a
GhostNappalm <m.baso@studenti.uniba.it>
2023-03-17 16:11:56 +0100

    dwdw

--- js/generalJS.js
+++ js/generalJS.js
@@ -10,7 +10,10 @@ $(document).ready(function(){
     });
     //
     /*
-
+$(".exit").click(function(){
+        return confirm("Sei sicuro di voler uscire?");
+    });
+    */
 
 
     //Js per la Mappa applicazione
@@ -58,4 +61,48 @@ $(document).ready(function(){
           $('#pop-up-exit').hide();
         });
 
+    //Js per la Mappa applicazione
+ /*   $('.veloGrigio').addClass('hidden');
+    $('.mappaApp').addClass('hidden');
+    $('.posAttuale').addClass('hidden');
+    $('.aiut').addClass('hidden');
+
+    $( ".mappaAppIcon , #closeBtn" ).click(function() {
+            if ($('.veloGrigio').hasClass('hidden')) {
+               $('.veloGrigio').removeClass('hidden');
+               $('header').css('z-index', '-5');
+               $('.mappaApp').removeClass('hidden');
+               $('.posAttuale').removeClass('hidden');
+            }
+            else{
+                $('.veloGrigio').addClass('hidden');
+                $('.mappaApp').addClass('hidden');
+                $('.posAttuale').addClass('hidden');
+                $('header').css('z-index', '1');
+              }
+              });
+
+
+            //Js per Help
+              $( ".help, #closeBtn_help").click(function() {
+                if ($('.veloGrigio').hasClass('hidden')) 
+                {
+                  $('.veloGrigio').removeClass('hidden');
+                  $('header').css('z-index', '-1');
+                  $('.aiut').removeClass('hidden');
+                }
+                else{
+                  $('.veloGrigio').addClass('hidden');
+                $('header').css('z-index', '1');
+                $('.aiut').addClass('hidden');
+                }
+              }); */
+
+
+    //
+
+
+    //Js per tasto esci
+    
+    //
 });
